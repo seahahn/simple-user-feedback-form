39c5b702d5e97e0dc7de78e35adbcb4af4f5c070
seahahn <seah.ahn.nt@gmail.com>
2024-01-16 17:35:49 +0100

    ✨ Home page updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import TopBar from "@/components/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TopBar />
+        {children}
+      </body>
     </html>
   );
 }
