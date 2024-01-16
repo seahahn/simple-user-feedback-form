618fefaf8493fb6d8776f790f2db7e0e0a1971da
seahahn <seah.ahn.nt@gmail.com>
2024-01-16 17:35:12 +0100

    💄 TopBar component created

---
+++ components/TopBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div className="fixed top-0 bg-primary-700 text-white w-full h-16 p-4 flex items-center">
+      <span className="text-3xl">Gyeongho Ahn</span>
+    </div>
+  );
+};
+
+export default TopBar;
