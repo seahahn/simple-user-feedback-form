48d00da9ddbafa1a2d2e1d4a59fbe71b2e227242
seahahn <seah.ahn.nt@gmail.com>
2024-01-16 22:00:50 +0100

    💄 BaseTextArea component created

---
+++ components/base/TextArea.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ITextArea extends React.ComponentProps<"textarea"> {}
+
+const BaseTextArea = (props: ITextArea) => {
+  return (
+    <textarea
+      id={props.id}
+      data-testid={props.id}
+      name={props.name}
+      className={props.className}
+      placeholder={props.placeholder}
+      onChange={props.onChange}></textarea>
+  );
+};
+
+export default BaseTextArea;
