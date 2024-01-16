0c77fd7072a3a964453afcfd1f5d0ef61330e130
seahahn <seah.ahn.nt@gmail.com>
2024-01-16 16:14:27 +0100

    🌱 constants - texts created

---
+++ constants/texts.ts
@@ -0,0 +1,8 @@
+const texts = {
+  APP_TITLE: "Simple User Feedback Form",
+  WELCOME_MESSAGE: `Thank you for using our website!
+  Your feedback is important to us as we strive to improve and provide you with the best experience.
+  Please take a few minutes to share your thoughts.`,
+};
+
+export default texts;
