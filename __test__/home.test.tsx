50011580a8e791bfe9dde61fec3b9fdaee3ddb66
seahahn <seah.ahn.nt@gmail.com>
2024-01-16 16:14:49 +0100

    🧪 Home page test created

---
+++ __test__/home.test.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+
+    const welcomeMsg = screen.getByRole("welcome-message");
+    expect(welcomeMsg).toBeInTheDocument();
+  });
+});
